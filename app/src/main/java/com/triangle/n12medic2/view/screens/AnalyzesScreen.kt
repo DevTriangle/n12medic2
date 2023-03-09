96d2b86c7f5fd828d0ce7ea6b13d7a21c1ed4a63
mobiledevelopment <thegoshagames@gmail.com>
2023-03-09 10:50:05 +0300

    s03 4

--- app/src/main/java/com/triangle/n12medic2/view/screens/AnalyzesScreen.kt
+++ app/src/main/java/com/triangle/n12medic2/view/screens/AnalyzesScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -18,6 +20,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.triangle.n12medic2.R
 import com.triangle.n12medic2.ui.components.*
 import com.triangle.n12medic2.ui.theme.captionColor
@@ -74,82 +78,92 @@ fun AnalyzesScreen(
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 20.dp)
+    val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = isLoading)
+
+    SwipeRefresh(
+        state = swipeRefreshState,
+        onRefresh = {
+            viewModel.loadNews()
+            viewModel.loadCatalog()
+        }
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 24.dp)
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
         ) {
-            AppTextField(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                value = searchValue,
-                onValueChange = { searchValue = it },
-                placeholder = {
-                    Text(
-                        text = "Искать анализы",
-                        fontSize = 16.sp,
-                        color = captionColor
-                    )
-                },
-                leadingIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_search),
-                        contentDescription = "",
-                        tint = descriptionColor
-                    )
-                }
-            )
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            AnimatedVisibility(visible = isNewsVisible) {
-                Column(modifier = Modifier.fillMaxWidth()) {
-                    BlockTitle(title = "Акции и новости")
-                    Spacer(modifier = Modifier.height(16.dp))
-                    LazyRow() {
-                        items(viewModel.news.distinct()) { news ->
-                            NewsComponent(news = news)
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            ) {
+                AppTextField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    value = searchValue,
+                    onValueChange = { searchValue = it },
+                    placeholder = {
+                        Text(
+                            text = "Искать анализы",
+                            fontSize = 16.sp,
+                            color = captionColor
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "",
+                            tint = descriptionColor
+                        )
+                    }
+                )
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                AnimatedVisibility(visible = isNewsVisible) {
+                    Column(modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState())) {
+                        BlockTitle(title = "Акции и новости")
+                        Spacer(modifier = Modifier.height(16.dp))
+                        LazyRow() {
+                            items(viewModel.news.distinct()) { news ->
+                                NewsComponent(news = news)
+                            }
                         }
                     }
+                    Spacer(modifier = Modifier.height(16.dp))
                 }
                 Spacer(modifier = Modifier.height(16.dp))
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            BlockTitle(title = "Каталог анализов")
-            Spacer(modifier = Modifier.height(16.dp))
-            LazyRow() {
-                items(viewModel.categories) { cat ->
-                    CategoryChip(
-                        name = cat,
-                        selected = selectedCategory == cat,
-                        onClick = { selectedCategory = it }
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
+                BlockTitle(title = "Каталог анализов")
+                Spacer(modifier = Modifier.height(16.dp))
+                LazyRow() {
+                    items(viewModel.categories) { cat ->
+                        CategoryChip(
+                            name = cat,
+                            selected = selectedCategory == cat,
+                            onClick = { selectedCategory = it }
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                    }
                 }
-            }
-            Spacer(modifier = Modifier.height(6.dp))
-            LazyColumn(
-                state = lazyListState
-            ) {
-                item {
-                    Spacer(modifier = Modifier.height(18.dp))
-                }
-                items(viewModel.analysis.filter { it.category.lowercase() == selectedCategory.lowercase() }.distinct()) { analysis ->
-                    AnalysisComponent(
-                        analysis = analysis,
-                        onClick = {
+                Spacer(modifier = Modifier.height(6.dp))
+                LazyColumn(
+                    state = lazyListState
+                ) {
+                    item {
+                        Spacer(modifier = Modifier.height(18.dp))
+                    }
+                    items(viewModel.analysis.filter { it.category.lowercase() == selectedCategory.lowercase() }.distinct()) { analysis ->
+                        AnalysisComponent(
+                            analysis = analysis,
+                            onClick = {
 
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
                 }
             }
         }
@@ -173,10 +187,6 @@ fun AnalyzesScreen(
                 )
             }
         )
-    }
-
-    if (isLoading) {
-        LoadingDialog()
     }
 }
 
