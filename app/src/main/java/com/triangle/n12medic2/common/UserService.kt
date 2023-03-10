2ff70a787c0e29f8f2eac4fbc276874463d63628
mobiledevelopment <thegoshagames@gmail.com>
2023-03-10 12:41:34 +0300

    s04 3

---
+++ app/src/main/java/com/triangle/n12medic2/common/UserService.kt
@@ -0,0 +1,48 @@
+package com.triangle.n12medic2.common
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.triangle.n12medic2.model.User
+
+// Класс для сохранения и получения данных о пациентах
+// Дата создания: 10.03.2023 12:15
+// Автор: Triangle
+class UserService {
+    fun loadPatients(sharedPreferences: SharedPreferences): MutableList<User> {
+        val users: MutableList<User> = ArrayList<User>().toMutableList()
+        val usersJson = Gson().fromJson(sharedPreferences.getString("users", "[]"), JsonArray::class.java)
+
+        usersJson.forEach { usr ->
+            val tUser = usr.asJsonObject
+
+            users.add(
+                User(
+                    tUser.get("firstname").asString,
+                    tUser.get("lastname").asString,
+                    tUser.get("middlename").asString,
+                    tUser.get("bith").asString,
+                    tUser.get("pol").asString,
+                    tUser.get("image").asString,
+                )
+            )
+        }
+
+        return users
+    }
+
+    fun savePatients(sharedPreferences: SharedPreferences, patients: MutableList<User>) {
+        val usersJson = Gson().toJson(patients)
+
+        with(sharedPreferences.edit()) {
+            putString("users", usersJson)
+            apply()
+        }
+    }
+
+    fun addMainPatient(sharedPreferences: SharedPreferences, patients: MutableList<User>, patient: User) {
+        val pList = patients
+
+        patients.add(0, patient)
+    }
+}
--- app/src/main/java/com/triangle/n12medic2/view/screens/ProfileScreen.kt
+++ app/src/main/java/com/triangle/n12medic2/view/screens/ProfileScreen.kt
@@ -1,34 +1,223 @@
 package com.triangle.n12medic2.view.screens
 
+import android.content.ContentValues
+import android.util.Log
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.skydoves.landscapist.glide.GlideImage
 import com.triangle.n12medic2.R
+import com.triangle.n12medic2.model.User
+import com.triangle.n12medic2.ui.components.AppButton
+import com.triangle.n12medic2.ui.components.AppTextField
+import com.triangle.n12medic2.ui.theme.captionColor
+import com.triangle.n12medic2.viewmodel.ManageCardViewModel
 
 // Экран "Профиль"
 // Дата создания: 09.03.2023 10:08
 // Автор: Triangle
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(
+    viewModel: ManageCardViewModel
+) {
+    val mContext = LocalContext.current
+    val sharedPreferences = mContext.getSharedPreferences("shared", ComponentActivity.MODE_PRIVATE)
+
+    val token = sharedPreferences.getString("token", "")
+
+    var firstName by rememberSaveable { mutableStateOf("") }
+    var patronymic by rememberSaveable { mutableStateOf("") }
+    var lastName by rememberSaveable { mutableStateOf("") }
+    var birthday by rememberSaveable { mutableStateOf("") }
+    var gender by rememberSaveable { mutableStateOf("") }
+
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+    var isLoading by rememberSaveable { mutableStateOf(false) }
+    var isErrorVisible by rememberSaveable { mutableStateOf(false) }
+
+    val userList: MutableList<User> = remember { mutableStateListOf() }
+
+    LaunchedEffect(Unit) {
+
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize(),
     ) {
-        Image(
+        Column(
             modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape)
-                .align(Alignment.Center),
-            painter = painterResource(id = R.drawable.ic_app_foreground),
-            contentDescription = ""
-        )
+                .widthIn(max = 400.dp)
+                .fillMaxHeight()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "Карта пациента",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(7.dp))
+            GlideImage(
+                imageModel =
+            )
+            Spacer(modifier = Modifier.height(7.dp))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "Без карты пациента вы не сможете заказать анализы.",
+                fontSize = 14.sp,
+                color = captionColor,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "В картах пациентов будут храниться результаты анализов вас и ваших близких.",
+                fontSize = 14.sp,
+                color = captionColor,
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            AppTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Имя",
+                        color = captionColor,
+                        fontSize = 15.sp
+                    )
+                },
+                value = firstName,
+                onValueChange = {
+                    firstName = it
+                }
+            )
+            Spacer(modifier = Modifier.height(18.dp))
+            AppTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Отчество",
+                        color = captionColor,
+                        fontSize = 15.sp
+                    )
+                },
+                value = patronymic,
+                onValueChange = {
+                    patronymic = it
+                }
+            )
+            Spacer(modifier = Modifier.height(18.dp))
+            AppTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Фамилия",
+                        color = captionColor,
+                        fontSize = 15.sp
+                    )
+                },
+                value = lastName,
+                onValueChange = {
+                    lastName = it
+                }
+            )
+            Spacer(modifier = Modifier.height(18.dp))
+            AppTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Дата рождения",
+                        color = captionColor,
+                        fontSize = 15.sp
+                    )
+                },
+                value = birthday,
+                onValueChange = {
+                    birthday = it
+                }
+            )
+            Spacer(modifier = Modifier.height(18.dp))
+            ExposedDropdownMenuBox(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                expanded = isExpanded,
+                onExpandedChange = {
+                    isExpanded = it
+                }
+            ) {
+                AppTextField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    placeholder = {
+                        Text(
+                            text = "Пол",
+                            color = captionColor,
+                            fontSize = 15.sp
+                        )
+                    },
+                    value = gender,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = {
+                        IconButton(
+                            onClick = { isExpanded = true }
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_expand),
+                                contentDescription = ""
+                            )
+                        }
+                    }
+                )
+                DropdownMenu(
+                    expanded = isExpanded,
+                    onDismissRequest = { isExpanded = false }
+                ) {
+                    DropdownMenuItem(
+                        onClick = { gender = "Мужской" }
+                    ) {
+                        Text(text = "Мужской")
+                    }
+                    DropdownMenuItem(
+                        onClick = { gender = "Женский" }
+                    ) {
+                        Text(text = "Женский")
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(48.dp))
+            AppButton(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                label = "Создать",
+                enabled = firstName.isNotBlank() && patronymic.isNotBlank() && lastName.isNotBlank() && birthday.isNotBlank() && gender.isNotBlank(),
+                onClick = {
+                    Log.d(ContentValues.TAG, "ManageCardScreen: $token")
+                    viewModel.createProfile(firstName, patronymic, lastName, birthday, gender, token!!)
+                    isLoading = true
+                }
+            )
+        }
     }
 }
